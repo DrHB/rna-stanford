a3f9c070b355c1ce14188ac6c32cbef3455776b8
drhb <habib.s.t.bukhari@gmail.com>
2023-09-23 00:51:14 +0000

    `exp_05`

--- rnacomp/dataset.py
+++ rnacomp/dataset.py
@@ -3,7 +3,8 @@
 # %% auto 0
 __all__ = ['good_luck', 'LenMatchBatchSampler', 'dict_to', 'to_device', 'DeviceDataLoader', 'encode_rna_sequence',
            'generate_edge_data', 'RNA_DatasetBaseline', 'RNA_DatasetBaselineSplit', 'RNA_DatasetV0', 'RNA_DatasetV1',
-           'RNA_DatasetV0G', 'generate_base_pair_matrix', 'RNA_DatasetBaselineSplitbppV0', 'RNA_Dataset_Test']
+           'RNA_DatasetV0G', 'generate_base_pair_matrix', 'RNA_DatasetBaselineSplitbppV0', 'dot_to_adjacency',
+           'RNA_DatasetBaselineSplitssV0', 'RNA_Dataset_Test', 'RNA_Dataset_TestBpp']
 
 # %% ../nbs/00_dataset.ipynb 2
 import pandas as pd
@@ -16,6 +17,7 @@ import seaborn as sbn
 import torch.nn.functional as F
 from tqdm import tqdm
 from sklearn.model_selection import KFold
+import random
 
 # %% ../nbs/00_dataset.ipynb 4
 def good_luck():
@@ -551,6 +553,71 @@ class RNA_DatasetBaselineSplitbppV0(Dataset):
         return {'seq':torch.from_numpy(seq), 'mask':mask, "adj_matrix": bpp}, \
                {'react':react, 'react_err':react_err,
                 'sn':sn, 'mask':mask}
+               
+def dot_to_adjacency(dot_notation, n):
+    adjacency_matrix = np.zeros((n, n), dtype=int)
+    
+    stack = []
+    for i, char in enumerate(dot_notation):
+        if char == '(':
+            stack.append(i)
+        elif char == ')':
+            j = stack.pop()
+            adjacency_matrix[i][j] = adjacency_matrix[j][i] = 1
+            
+    return adjacency_matrix
+               
+class RNA_DatasetBaselineSplitssV0(Dataset):
+    def __init__(self, df, mode='train', seed=2023, fold=0, nfolds=4, mask_only=False, 
+                 sn_train=True, **kwargs):
+        """
+        short sequence without adapters 
+        """
+        self.seq_map = {'A':0,'C':1,'G':2,'U':3}
+        self.Lmax = 206 - 26 - 21
+        df['L'] = df.sequence.apply(len)
+        df_2A3 = df.loc[df.experiment_type=='2A3_MaP'].reset_index(drop=True)
+        df_DMS = df.loc[df.experiment_type=='DMS_MaP'].reset_index(drop=True)
+        
+        if mode != 'train' or sn_train:
+            m = (df_2A3['SN_filter'].values > 0) & (df_DMS['SN_filter'].values > 0)
+            df_2A3 = df_2A3.loc[m].reset_index(drop=True)
+            df_DMS = df_DMS.loc[m].reset_index(drop=True)
+        
+        self.seq = df_2A3['sequence'].values
+        self.ss = df_2A3['ss_roi'].values
+        self.L = df_2A3['L'].values
+        self.react_2A3 = df_2A3[[c for c in df_2A3.columns if 'reactivity_0' in c]].values
+        self.react_DMS = df_DMS[[c for c in df_DMS.columns if 'reactivity_0' in c]].values
+        self.react_err_2A3 = df_2A3[[c for c in df_2A3.columns if 'reactivity_error_0' in c]].values
+        self.react_err_DMS = df_DMS[[c for c in df_DMS.columns if 'reactivity_error_0' in c]].values
+        self.sn_2A3 = df_2A3['signal_to_noise'].values
+        self.sn_DMS = df_DMS['signal_to_noise'].values
+        self.mask_only = mask_only
+        
+    def __len__(self):
+        return len(self.seq)  
+    
+    def __getitem__(self, idx):
+        seq = self.seq[idx][26:-21]
+        if self.mask_only:
+            mask = torch.zeros(self.Lmax, dtype=torch.bool)
+            mask[:len(seq)] = True
+            return {'mask':mask},{'mask':mask}
+        seq = [self.seq_map[s] for s in seq]
+        seq = np.array(seq)
+        mask = torch.zeros(self.Lmax, dtype=torch.bool)
+        mask[:len(seq)] = True
+        seq = np.pad(seq,(0,self.Lmax-len(seq)))
+        bpp = torch.tensor(dot_to_adjacency(self.ss[idx], self.Lmax)).int()
+        
+        react = torch.from_numpy(np.stack([self.react_2A3[idx],self.react_DMS[idx]],-1))[26:-21]
+        react_err = torch.from_numpy(np.stack([self.react_err_2A3[idx],self.react_err_DMS[idx]],-1))[26:-21]
+        sn = torch.FloatTensor([self.sn_2A3[idx],self.sn_DMS[idx]])
+        
+        return {'seq':torch.from_numpy(seq), 'mask':mask, "adj_matrix": bpp}, \
+               {'react':react, 'react_err':react_err,
+                'sn':sn, 'mask':mask}
 
 
 # %% ../nbs/00_dataset.ipynb 10
@@ -579,5 +646,34 @@ class RNA_Dataset_Test(Dataset):
         ids = np.pad(ids,(0,self.Lmax-L), constant_values=-1)
         
         return {'seq':torch.from_numpy(seq), 'mask':mask}, \
+               {'ids':ids}
+               
+               
+class RNA_Dataset_TestBpp(Dataset):
+    def __init__(self, df, mask_only=False, **kwargs):
+        self.seq_map = {'A':0,'C':1,'G':2,'U':3}
+        df['L'] = df.sequence.apply(len)
+        self.Lmax = df['L'].max()
+        self.df = df
+        self.mask_only = mask_only
+        
+    def __len__(self):
+        return len(self.df)  
+    
+    def __getitem__(self, idx):
+        id_min, id_max, seq = self.df.loc[idx, ['id_min','id_max','sequence']]
+        mask = torch.zeros(self.Lmax, dtype=torch.bool)
+        L = len(seq)
+        mask[:L] = True
+        if self.mask_only: return {'mask':mask},{}
+        ids = np.arange(id_min,id_max+1)    
+        seq = [self.seq_map[s] for s in seq]
+        seq = np.array(seq)
+        seq = np.pad(seq,(0,self.Lmax-L))
+        ids = np.pad(ids,(0,self.Lmax-L), constant_values=-1)
+        bpp = self.df['bpp'][idx]
+        bpp = (generate_base_pair_matrix(bpp, self.Lmax) > 0.5).int()
+        
+        return {'seq':torch.from_numpy(seq), 'mask':mask,  "adj_matrix": bpp}, \
                {'ids':ids}
 
